b29c665280d42f17d0972a91764f33c43d793bcd
yunji991004 <yungee1004@naver.com>
2021-12-04 06:11:20 +0900

    totalfile

--- app/src/main/java/com/example/login/HomeActivity.java
+++ app/src/main/java/com/example/login/HomeActivity.java
@@ -8,23 +8,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeActivity extends Activity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+
         Button btn = (Button)findViewById(R.id.button);
+        Button btn2 = (Button)findViewById(R.id.button2);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(HomeActivity.this, Reservation.class);
                 startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                intent.putExtra("userID",userID);
+                startActivity(intent);
+
             }
         });
 
--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tv_id,tv_pw;
+    private TextView tv_id,tv_num;
     private Button button;
 
     @Override
@@ -18,14 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv_id = findViewById(R.id.tv_id);
-        tv_pw = findViewById(R.id.tv_pw);
+        tv_num = findViewById(R.id.tv_num);
 
-        //아이디와 비밀번호가 메인액티비티까지 정상적으로 도착
         Intent intent = getIntent();
         String userID = intent.getStringExtra("userID");
-        String userPassword = intent.getStringExtra("userPassword");
 
         tv_id.setText(userID);
-        tv_pw.setText(userPassword);
+        //tv_num.setText();
+
     }
 }
